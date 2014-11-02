18e58af121c8735ddc43f27b4a758588b06767c2
Kaiyin Zhong <kindlychung@gmail.com>
2014-11-02 22:15:48 +0100

    crt1 max shift number option

--- R/crt1.R
+++ R/crt1.R
@@ -1,12 +1,13 @@
-crt1 = function(wDir=".", taskName, plinkArgs, initGwasArgs, initGwas=FALSE, traitType=c("bin", "con"), pFilter=.05) {
+crt1 = function(wDir=".", taskName, plinkArgs, initGwasArgs, initGwas=FALSE, traitType=c("bin", "con"), pFilter=.05, nMaxShift) {
     traitType = match.arg(traitType)
     setwd(wDir)
     hubcollr = collrinfo()
     bedcollinfo(hubcollr, genbed = FALSE)
     taskinfo(hubcollr, taskName, plinkArgs, initGwasArgs, initGwas, traitType)
     hubtask = hubcollr[[taskName]]
-    taskBedsPlinkOut(hubcollr, taskName, hubtask$fullGwasOut, pFilter, 1, 10)
+    taskBedsPlinkOut(hubcollr, taskName, hubtask$fullGwasOut, pFilter, 1, nMaxShift)
     taskAnalyze(hubcollr, taskName)
     readcoll.task(hubcollr, taskName)
-    contrastPlot(hubcollr[[taskName]])
+    hubtask$contrastPlotRes = contrastPlot(hubcollr[[taskName]])
+    hubtask$contrastPlotRes
 }
