8f20a66a036ca9599e6071173e3767d56aff18ed
Kaiyin Zhong <kindlychung@gmail.com>
2014-12-09 00:14:10 +0100

    a

--- R/checkFileExists.R
+++ R/checkFileExists.R
@@ -1,3 +1,10 @@
-checkFileExists = function(filename) {
-    if(!file.exists(filename)) stop(sprintf("%s does not exist.", filename))
+checkFileExists = function(filenames) {
+    if(!is.character(filenames)) {
+        stop("filenames must be a character vector.")
+    }
+    nonExistFiles = filenames[!file.exists(filenames)]
+    if(length(nonExistFiles) > 0) {
+        stop(sprintf("Non-existent file(s): \n%s", paste(nonExistFiles, collapse="\n")))
+    }
+    invisible(NULL)
 }
