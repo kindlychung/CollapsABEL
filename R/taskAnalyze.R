88cd1809b86f330095797feca1c3cba4c382e18e
Kaiyin Zhong <kindlychung@gmail.com>
2014-12-09 17:00:18 +0100

    a

--- R/taskAnalyze.R
+++ R/taskAnalyze.R
@@ -1,6 +1,6 @@
 taskAnalyze = function(hub, taskName) {
     hubtask = hub[[taskName]]
-    tmplist = hubtask$plinkParamList
+    tmplist = hubtask$plinkArgs
     tmplist$plinkcollFileStems = hubtask$shiftFilesStem
     do.call(plinkcollr, tmplist)
     cleanLog(hubtask$taskPath)
