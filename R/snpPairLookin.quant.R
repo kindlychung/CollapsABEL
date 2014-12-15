e4736265f92a0c7e4cff34e064b5f4c158d97540
Kaiyin Zhong <kindlychung@gmail.com>
2014-12-15 22:11:01 +0100

    look into snp pair in detail

---
+++ R/snpPairLookin.quant.R
@@ -0,0 +1,20 @@
+## require(ggplot2)
+## n_simu = 5000
+## g1 = sample(0:2, n_simu, replace = TRUE)
+## g2 = sample(0:2, n_simu, replace = TRUE)
+## g = paste(g1, g2, sep = "")
+## g = factor(g, levels = c("00", "10", "01", "20", "11", "02", "21", "12", "22"))
+## pheno = rnorm(n_simu)
+## dat = data.frame(geno=g, pheno=pheno)
+## ggplot(dat, aes(x = pheno)) + geom_density() + facet_grid(geno ~ .)
+## require(rbed2)
+## remove.packages("rbed2")
+## require(devtools)
+## install_bitbucket("kindlychung/rbed2")
+
+snpPairLookin.quant = function(g1, g2, pheno) {
+    g = paste(g1, g2, sep = "")
+    g = factor(g, levels = c("00", "10", "01", "20", "11", "02", "21", "12", "22"))
+    dat = data.frame(geno=g, pheno=pheno)
+    ggplot(dat, aes(x = pheno)) + geom_density() + facet_grid(geno ~ .)
+}
