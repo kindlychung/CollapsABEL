6882c2ddd7ebf0509529c59248b3accfab20bc9f
Kaiyin Zhong <kindlychung@gmail.com>
2015-01-04 11:43:15 +0100

    normalized histogram

--- R/snpPairLookin.quant.R
+++ R/snpPairLookin.quant.R
@@ -19,7 +19,7 @@ snpPairLookin.quant = function(g1, g2, pheno) {
     dat = mice::cc(dat)
     # normalized histogram, sum + .1, to avoid division-by-zero error
     ## ggplot(dat, aes(x = pheno)) + geom_histogram(aes(y=..count../(0.1 + sum(..count..)))) + facet_grid(. ~ geno)
-    ggplot(dat, aes(x = pheno)) + geom_histogram() + facet_grid(. ~ geno) + scale_y_continuous(labels = percent_format())
+    ggplot(dat, aes(x = pheno, y = ..ncount..)) + geom_histogram() + facet_grid(. ~ geno) + scale_y_continuous(labels = percent_format())
 }
 
 ## load("/tmp/snpdat5.rda")
