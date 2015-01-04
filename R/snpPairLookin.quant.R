83470a5efaba54c36e6ba2f484ea93a58968ce85
Kaiyin Zhong <kindlychung@gmail.com>
2015-01-04 11:31:51 +0100

    normalized histogram

--- R/snpPairLookin.quant.R
+++ R/snpPairLookin.quant.R
@@ -17,5 +17,25 @@ snpPairLookin.quant = function(g1, g2, pheno) {
     g = factor(g, levels = c("00", "10", "01", "20", "11", "02", "21", "12", "22"))
     dat = data.frame(geno=g, pheno=pheno)
     dat = mice::cc(dat)
-    ggplot(dat, aes(x = pheno)) + geom_histogram(aes(y=..count../(sum(..count..)))) + facet_grid(. ~ geno)
+    # normalized histogram, sum + .1, to avoid division-by-zero error
+    ggplot(dat, aes(x = pheno)) + geom_histogram(aes(y=..count../(0.1 + sum(..count..)))) + facet_grid(. ~ geno)
 }
+
+## load("/tmp/snpdat5.rda")
+## load("/tmp/snpdat18.rda")
+## setwd("/media/thecus/kaiyin/RS123_1KG_maf0.01/")
+## snpdat5 = snpdat5[, !(names(snpdat5) %in% c("chr", "bp", "chr2", "bp2", "pbonfer"))]
+## require(rbed2)
+## rbinfo = rbedinfo("RS123")
+## geno_mat = bedmat.snpNameVec(rbinfo, c("rs450917", "rs77193767"))
+## pheno_mat = read.table("/media/data1/RS123_pheno/dermatology.csv", head=T)
+## pheno_mat[pheno_mat == 9999] = NA
+## pheno_mat = pheno_mat[, c("FID", "IID", "Sagging")]
+## pheno_mat$IID = as.character(pheno_mat$IID)
+## require(dplyr)
+## dat = inner_join(pheno_mat, geno_mat, by="IID")
+## require(collr2)
+## p = snpPairLookin.quant(dat$rs77193767, dat$rs450917, dat$Sagging)
+## require(ggplot2)
+## ggsave(plot=p, "/tmp/snppair_lookin_rs77193767_rs450917.pdf", width=10, height=5)
+## savehistory(file="/tmp/x.r")
