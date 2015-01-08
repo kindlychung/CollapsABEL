6c7ebc59277de2dbd24a6413e994ba5b6eb82ca4
Kaiyin Zhong <kindlychung@gmail.com>
2015-01-08 16:57:03 +0100

    don't clean log files

--- R/taskAnalyze.R
+++ R/taskAnalyze.R
@@ -3,5 +3,5 @@ taskAnalyze = function(hub, taskName) {
     tmplist = hubtask$plinkArgs
     tmplist$plinkcollFileStems = hubtask$shiftFilesStem
     do.call(plinkcollr, tmplist)
-    cleanLog(hubtask$taskPath)
+    # cleanLog(hubtask$taskPath)
 }
