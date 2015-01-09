22b454ee18da9d3072966d6984539f554e0d4f0d
Kaiyin Zhong <kindlychung@gmail.com>
2015-01-09 18:11:44 +0100

    bugfix

--- R/rep_models.R
+++ R/rep_models.R
@@ -1,7 +1,6 @@
 ## Given a pair of SNPs, fit a model using single and collapsed genotypes
 rep_models = function(mat, snp1, snp2, pheno_name, covar_names) {
     mat$colgen = ((mat[[snp1]] + mat[[snp2]]) >= 2) + 0
-    n = length(covar_names) + 2
     covar_names = c(covar_names, "+")
     names(covar_names)[length(covar_names)] = "sep"
     covars = do.call(paste, as.list(covar_names))
@@ -10,19 +9,19 @@ rep_models = function(mat, snp1, snp2, pheno_name, covar_names) {
     mod3 = summary(glm(as.formula(sprintf("%s ~ %s + colgen", pheno_name, covars)), data=mat))
 
     if(snp1 %in% mod1$coef) {
-        p1 = mod1$coefficients[n, 4]
+        p1 = mod1$coefficients[snp1, 4]
     } else {
         p1 = NA
     }
 
     if(snp2 %in% mod2$coef) {
-        p2 = mod2$coefficients[n, 4]
+        p2 = mod2$coefficients[snp2, 4]
     } else {
         p2 = NA
     }
 
     if("colgen" %in% mod3$coef) {
-        p3 = mod3$coefficients[n, 4]
+        p3 = mod3$coefficients["colgen", 4]
     } else {
         p3 = NA
     }
