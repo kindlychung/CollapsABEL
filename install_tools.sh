85e595c6c893266afe79da8d37ebef6aae7612d4
Kaiyin Zhong <kindlychung@gmail.com>
2015-02-09 17:39:18 +0100

    install script

--- install_tools.sh
+++ install_tools.sh
@@ -3,24 +3,24 @@
 # install required packages
 if [[ $(uname -a) == *"Ubuntu"* ]]; then
     export DEBIAN_FRONTEND=noninteractive
-    sudo apt-get -q -y build-dep libcurl4-gnutls-dev
-    sudo apt-get -q -y install libcurl4-gnutls-dev
-    sudo apt-get -q -y install \
+     apt-get -q -y build-dep libcurl4-gnutls-dev
+     apt-get -q -y install libcurl4-gnutls-dev
+     apt-get -q -y install \
         r-base git \
         libboost-dev \
         libboost-system-dev libboost-filesystem-dev libboost-program-options-dev \
         libarpack2-dev liblapack-dev libatlas-dev libopenblas-dev \
-        cmake
+        cmake unzip
 elif [[ $(ls /etc/*-release) == *"fedora-release"*  ]]; then
-    sudo yum -y install \
+     yum -y install \
         R git libcurl libcurl-devel \
         boost-devel \
         boost-system boost-filesystem boost-program-options \
         lapack arpack atlas openblas \
         lapack-devel arpack-devel atlas-devel openblas-devel \
-        cmake
+        cmake unzip
 elif [[ $(ls /etc/*-release) == *"SuSE-release"*  ]]; then
-    sudo zypper -n install \
+     zypper -n install \
         R-base libcurl4 libcurl-devel git gcc-c++ \
         boost-devel \
         libboost_system* libboost_filesystem* libboost_program_options* \
@@ -28,7 +28,7 @@ elif [[ $(ls /etc/*-release) == *"SuSE-release"*  ]]; then
         libarpack* arpack-ng-devel \
         libatlas3 libatlas3-devel  \
         libopenblas* openblas-devel \
-        cmake
+        cmake unzip
 else
     echo "Linux distribution not recognized."
     exit
@@ -41,7 +41,7 @@ tar zvxf armadillo-4.600.4.tar.gz
 cd armadillo-4.600.4/
 cmake .
 make
-sudo make install
+make install
 cd /tmp
 rm -rf armadillo-4.600.4*
 
@@ -50,8 +50,17 @@ cd /tmp
 git clone https://kindlychung@bitbucket.org/kindlychung/bedcoll.git
 cd bedcoll/
 ./build.sh
-sudo cp bedcoll /usr/local/bin/
+cp bedcoll /usr/local/bin/
 cd /tmp
 rm -rf bedcoll*
 
+# install PLINK2
+cd /tmp
+wget https://www.cog-genomics.org/static/bin/plink150209/plink_linux_x86_64.zip
+mkdir plink2
+unzip plink_linux_x86_64.zip -d plink2
+cd plink2
+cp -av plink prettify /usr/local/bin
+cd /tmp
+rm -rf plink*
 
